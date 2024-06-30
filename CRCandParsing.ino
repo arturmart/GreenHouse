47df49f8009fbe81fc5905504fa2acedad5fd85a
arturmart <artur.mart2002@gmail.com>
2024-06-30 19:19:49 +0400

    Update CRCandParsing.ino
    + Rele

--- CRCandParsing.ino
+++ CRCandParsing.ino
@@ -1,5 +1,5 @@
 #include <Arduino_CRC32.h>
-
+#include <GParser.h>
 
 const unsigned int BUFFER_SIZE = 64;  // Размер буфера для хранения входящих данных
 char inputBuffer[BUFFER_SIZE];        // Буфер для входящих данных
@@ -10,8 +10,94 @@ Arduino_CRC32 crc32;
 #define DELIMITER2L ','
 #define TERMINATOR '\n'
 
+const int RELAY_COUNT2 = 8;
+int relayPin2[RELAY_COUNT2] = {2,3,4,5,6,7,8,9};                 // Светодиод подключенный к вход/выходу 13
+
+
+class RelayController{
+  public:
+    RelayController(int* _relayPin,const int _RELAY_COUNT):RELAY_COUNT( _RELAY_COUNT ){
+      setRelayPin(_relayPin);
+      initRelay();
+      //testRelay();
+
+    }
+    RelayController(){
+
+    }
+
+    RelayController(const RelayController& source): RELAY_COUNT (source.RELAY_COUNT ){
+      
+      relayPin = new int[RELAY_COUNT];
+      for(int i =0;i<RELAY_COUNT;i++){
+        relayPin[i]=source.relayPin[i];
+      }
+
+    }
+    ~RelayController(){
+      delete(relayPin);
+    }
+    RelayController& operator=(const RelayController& source){
+      RELAY_COUNT = source.RELAY_COUNT;
+
+      relayPin = new int[RELAY_COUNT];
+      for(int i =0;i<RELAY_COUNT;i++){
+        relayPin[i]=source.relayPin[i];
+      }
+
+      return *this;
+    }
+
+    void setRelayPin(int* _relayPin){
+      relayPin = _relayPin;
+
+    }
+     
+    void relayWrite(int id, bool dig){
+      digitalWrite(relayPin[id], !dig);
+    }
+    void initRelay(){
+      for(int i =0;i<RELAY_COUNT;i++){
+          pinMode(relayPin[i], OUTPUT);
+          relayWrite(i,LOW);
+        }
+    }
+    void testRelay(){
+      for(int i =0;i<RELAY_COUNT;i++){
+        relayWrite(i,HIGH);
+        delay(300);
+        relayWrite(i,LOW);
+        delay(100);
+      }
+    }
+
+    void Log(){
+      Serial.print("size:");
+      Serial.println(RELAY_COUNT);
+
+      for(int i =0;i<RELAY_COUNT;i++){
+        Serial.print(relayPin[i]);
+
+        Serial.print(" ");
+      }
+      Serial.println();
+    }
+
+  private:
+    int* relayPin;
+    int RELAY_COUNT;
+  };
+
+bool getBit(unsigned char byte, int position) {
+    return (byte >> position) & 0x1;
+  }
+
+
+RelayController RC;
+
 
 void setup() {
+  RC =  RelayController(relayPin2,RELAY_COUNT2);
   Serial.begin(9600);  // Инициализация Serial со скоростью 9600 бод
 }
 
@@ -57,28 +143,23 @@ bool checkCRC(char* buf, int size){
 
 void parsing(char* buf, int size) {
 
-  Serial.print("InputBuf[");Serial.print(size);Serial.print("]=");Serial.println(buf);
-
-
+    Serial.print("InputBuf[");Serial.print(size);Serial.print("]=");Serial.println(buf);
+    
+    GParser data(buf, DELIMITER2L);
+    int dataSize = data.split();
   
-/*
-  //char qaq[4] = data1L[1];
-  uint32_t receivedCRC = strtoul("iÃÆ\0", NULL, 16);  // Преобразование CRC из строки в число
+    //for(int i =0 ;i<dataSize;i++)Serial.println(data[i]);
+  for(int i =0;i<dataSize;i++){
+    if(data.equals(i,"R")) {
+        RC.relayWrite(data.getInt(i+1),data.getInt(i+2));
+        i+=2;
+        }
+    
+      }
+    
 
-  CRC32 crc;
-  crc.update(data1L[0], strlen(data1L[0]));
-  unsigned long calculatedCRC = crc.finalize();
+    
 
- 
-  Serial.println( receivedCRC);
-  Serial.println(calculatedCRC);*/
-  //return receivedCRC == calculatedCRC;  // Сравнение полученного и вычисленного CRC
-
-
-  //GParser data2L(data1L[0], DELIMITER2L);
-  //int parseCount2L = data2L.split();
-
-  //for (byte i = 0; i < parseCount2L; i++) Serial.println(data2L[i]);
-}
+  }
 
 
