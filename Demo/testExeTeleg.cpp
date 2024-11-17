56237e8c1e6886969027b3c4a0749925914f6303
arturmart <artur.mart2002@gmail.com>
2024-11-18 01:55:09 +0400

    Update testExeTeleg.cpp

--- Demo/testExeTeleg.cpp
+++ Demo/testExeTeleg.cpp
@@ -2,18 +2,114 @@
 #include "Executor.h"
 #include <iostream>
 
+
 //g++ -std=c++17 -o test testExeTeleg.cpp -L/usr/local/include  -lTgBot -lboost_system -lssl -lcrypto -lpthread Executor.cpp  DataParsing.cpp TreeParsing.cpp MessageQueuing.cpp SerialComm.cpp -lwiringPi DataCombine.cpp crcCalc.cpp -lz
 
 Executor EXE;
 
 int main() {
     // Замените "YOUR_BOT_TOKEN" на ваш реальный токен бота
-    std::string token = "7804127004:AAEQkzTISnsoBpESYJQdVERP6gX10d6rA1c";
+    std::string token = "---";
     TgBot::Bot bot(token);
+
+    
+
+
 
     // Обработчик команды "/start"
     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "Добро пожаловать в бота! Напишите /help для списка команд.");
+        TgBot::InlineKeyboardMarkup::Ptr keyboard(new TgBot::InlineKeyboardMarkup);
+
+        TgBot::InlineKeyboardButton::Ptr button1On(new TgBot::InlineKeyboardButton);
+        TgBot::InlineKeyboardButton::Ptr button2On(new TgBot::InlineKeyboardButton);
+        TgBot::InlineKeyboardButton::Ptr button3On(new TgBot::InlineKeyboardButton);
+        TgBot::InlineKeyboardButton::Ptr button4On(new TgBot::InlineKeyboardButton);
+
+        TgBot::InlineKeyboardButton::Ptr button1Off(new TgBot::InlineKeyboardButton);
+        TgBot::InlineKeyboardButton::Ptr button2Off(new TgBot::InlineKeyboardButton);
+        TgBot::InlineKeyboardButton::Ptr button3Off(new TgBot::InlineKeyboardButton);
+        TgBot::InlineKeyboardButton::Ptr button4Off(new TgBot::InlineKeyboardButton);
+
+        button1On->text = "Rele 1 On";
+        button1On->callbackData = "button_click_Rele1On";
+
+        button2On->text = "Rele 2 On";
+        button2On->callbackData = "button_click_Rele2On";
+
+        button3On->text = "Rele 3 On";
+        button3On->callbackData = "button_click_Rele3On";
+
+        button4On->text = "Rele 4 On";
+        button4On->callbackData = "button_click_Rele4On";
+
+        button1Off->text = "Rele 1 Off";
+        button1Off->callbackData = "button_click_Rele1Off";
+
+        button2Off->text = "Rele 2 Off";
+        button2Off->callbackData = "button_click_Rele2Off";
+
+        button3Off->text = "Rele 3 Off";
+        button3Off->callbackData = "button_click_Rele3Off";
+
+        button4Off->text = "Rele 4 Off";
+        button4Off->callbackData = "button_click_Rele4Off";
+
+        keyboard->inlineKeyboard.push_back({button1On,button2On,button3On,button4On});
+        keyboard->inlineKeyboard.push_back({button1Off,button2Off,button3Off,button4Off});
+
+        bot.getApi().sendMessage(message->chat->id, "Добро пожаловать в бота! Напишите /help для списка команд.", nullptr, 0, keyboard);
+    });
+
+    bot.getEvents().onCallbackQuery([&bot](TgBot::CallbackQuery::Ptr query) {
+        if (query->data == "button_click_Rele1On") {
+            bot.getApi().sendMessage(query->message->chat->id, "RL1 On");
+
+            EXE.queuePush("83,0,1");
+            EXE.update();
+        } 
+        else if (query->data == "button_click_Rele2On") {
+            bot.getApi().sendMessage(query->message->chat->id, "RL2 On");
+
+            EXE.queuePush("83,1,1");
+            EXE.update();
+        } 
+        else if (query->data == "button_click_Rele3On") {
+            bot.getApi().sendMessage(query->message->chat->id, "RL3 On");
+
+            EXE.queuePush("83,2,1");
+            EXE.update();
+        } 
+        else if (query->data == "button_click_Rele4On") {
+            bot.getApi().sendMessage(query->message->chat->id, "RL4 On");
+
+            EXE.queuePush("83,3,1");
+            EXE.update();
+        } 
+        else if (query->data == "button_click_Rele1Off") {
+            bot.getApi().sendMessage(query->message->chat->id, "RL1 Off");
+
+            EXE.queuePush("83,0,0");
+            EXE.update();
+        } 
+        else if (query->data == "button_click_Rele2Off") {
+            bot.getApi().sendMessage(query->message->chat->id, "RL2 Off");
+
+            EXE.queuePush("83,1,0");
+            EXE.update();
+        } 
+        else if (query->data == "button_click_Rele3Off") {
+            bot.getApi().sendMessage(query->message->chat->id, "RL3 Off");
+
+            EXE.queuePush("83,2,0");
+            EXE.update();
+        } 
+        else if (query->data == "button_click_Rele4Off") {
+            bot.getApi().sendMessage(query->message->chat->id, "RL4 Off");
+
+            EXE.queuePush("83,3,0");
+            EXE.update();
+        } 
+
     });
 
     // Обработчик команды "/help"
@@ -70,23 +166,4 @@ int main() {
     return 0;
 }
 
-/*
-#include "Executor.h"
 
-//g++ -o test  testExeTeleg.cpp Executor.cpp  DataParsing.cpp TreeParsing.cpp MessageQueuing.cpp SerialComm.cpp -lwiringPi DataCombine.cpp crcCalc.cpp -lz
-
-Executor EXE;
-
-int main(){
-
-   for(int i = 0; i<10;i++){
-      EXE.queuePushTurnOffAll();
-      EXE.queuePushTurnOnAll();
-   }
-
-   while(true){
-      EXE.update();
-   }
-
-}
-*/
