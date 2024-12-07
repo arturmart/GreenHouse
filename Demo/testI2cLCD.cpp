44f85253705d8efe7d0aba715df09c87dd331dcf
arturmart <artur.mart2002@gmail.com>
2024-12-08 00:49:58 +0400

    test LCD I2C

---
+++ Demo/testI2cLCD.cpp
@@ -0,0 +1,31 @@
+#include "I2CLCD.h"
+#include <iostream>
+#include <string>
+
+
+I2CLCD lcd(0x3f);
+
+int main(){
+   std::string input;
+
+   if(lcd.initialize()){
+      std::cout<<"LCD is Inited!"<<std::endl;
+   }
+   else{
+      std::cout<<"ERROR LCD!"<<std::endl;
+      return 0;
+   }
+
+   while (true)
+   {
+      
+      std::cout<<"sand Message:";
+      std::cin>>input;
+      lcd.clear();
+      std::cout<<">"<<input.c_str()<<std::endl;
+      lcd.displayString(input.c_str(), 1);
+   }
+   
+
+}
+
